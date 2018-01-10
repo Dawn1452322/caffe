c6878b2941ee82cf5e50e5096fed96f2975c2923
Dawn1452322 <30343000+Dawn1452322@users.noreply.github.com>
2018-01-10 18:39:19 +0800

    Update fetch_cached_data.sh

--- cache/fetch_cached_data.sh
+++ cache/fetch_cached_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#----------进入脚本文件所在目录----------#
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+#-------------------------------------#
 
-DATASET_NAME=$1
+DATASET_NAME=$1 #“$1”表示第一个位置参数
 echo "Downloading $DATASET_NAME ground truth data"
 
 wget http://pascal.inrialpes.fr/data2/act-detector/downloads/cache/$DATASET_NAME-GT.pkl
