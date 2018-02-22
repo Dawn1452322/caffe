d64bdffb7be23ffda1dddabc8962f368119fdddb
Dawn1452322 <30343000+Dawn1452322@users.noreply.github.com>
2018-02-22 11:56:59 +0800

    Update ACT_datalayer.py

--- act-detector-scripts/ACT_datalayer.py
+++ act-detector-scripts/ACT_datalayer.py
@@ -40,7 +40,25 @@ batch_samplers = [{
     'max_sample': 1,
 },{
     'sampler': {'min_scale': 0.3, 'max_scale': 1.0, 'min_aspect_ratio': 0.5, 'max_aspect_ratio': 2.0,},
-    'sample_constraint':
+    'sample_constraint': {'min_jaccard_overlap': 0.5,},
+    'max_trials': 50,
+    'max_sample': 1,
+},{
+    'sampler': {'min_scale': 0.3, 'max_scale': 1.0, 'min_aspect_ratio': 0.5, 'max_aspect_ratio': 2.0,},
+    'sample_constraint': {'min_jaccard_overlap': 0.7,},
+    'max_trials': 50,
+    'max_sample': 1,
+},{
+    'sampler': {'min_scale': 0.3, 'max_scale': 1.0, 'min_aspect_ratio': 0.5, 'max_aspect_ratio': 2.0,},
+    'sample_constraint': {'min_jaccard_overlap': 0.9,},
+    'max_trials': 50,
+    'max_sample': 1,
+},{
+    'sampler': {'min_scale': 0.3, 'max_scale': 1.0, 'min_aspect_ratio': 0.5, 'max_aspect_ratio': 2.0,},
+    'sample_constraint': {'max_jaccard_overlap': 1.0,},
+    'max_trials': 50,
+    'max_sample': 1,
+},]
 
 #随机修改亮度
 def random_brightness(imglist, brightness_prob, brightness_delta):
